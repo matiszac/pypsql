e5aed63c4c0e9c47f5413a032b8333d0a7d00184
matiszac <zcornilliac@gmail.com>
2025-07-29 10:53:25 -0400

    feat: astnodes

--- main.py
+++ main.py
@@ -3,42 +3,61 @@ from pypsql import Table, Field, Query
 def main():
     InventoryCosts = Table('InventoryCosts')
     TransDate = Field('TransDate', 'text', InventoryCosts)
-    ItemRecNumber = Field('ItemRecNumber', 'number', InventoryCosts)
-    Quantity = Field('Quantity', 'number', InventoryCosts)
-    CostAcctRecNumber = Field('CostAcctRecNumber', 'number', InventoryCosts)
-    RecordType = Field('RecordType', 'number', InventoryCosts)
+    ItemRecNumber = Field('ItemRecNumber', 'int', InventoryCosts)
+    Quantity = Field('Quantity', 'decimal', InventoryCosts)
+    CostAcctRecNumber = Field('CostAcctRecNumber', 'int', InventoryCosts)
+    RecordType = Field('RecordType', 'int', InventoryCosts)
     
     LineItem = Table('LineItem')
-    ItemIsInactive = Field('ItemIsInactive', 'number', LineItem)
-    ItemRecordNumber = Field('ItemRecordNumber', 'number', LineItem)
+    ItemIsInactive = Field('ItemIsInactive', 'int', LineItem)
+    ItemRecordNumber = Field('ItemRecordNumber', 'int', LineItem)
     StockingUM = Field('StockingUM', 'text', LineItem)
     ItemID = Field('ItemID', 'text', LineItem)
 
-    sql = Query(
+    # lim1 -> select
+    # irs4 -> inner
+    # irs1 -> select
+    # irs3 -> inner
+    # irs2 -> select
+    # irs2 -> where
+    # irs2 -> group
+    # irs3 -> on
+    # irs1 -> where
+    # irs4 -> on
+    # lim1 -> where
+    # lim1 -> order
+
+    select = (
         LineItem
-            .select(ItemID, StockingUM, ItemIsInactive)
-            .inner(InventoryCosts
-                .select(ItemRecNumber, TransDate, Quantity, CostAcctRecNumber, RecordType)
-                .inner(InventoryCosts
-                    .select(ItemRecNumber, RecordType, TransDate.MAX)
-                    .where(RecordType == 50)
-                    .group(ItemRecNumber)
-                ).on(
-                    ItemRecNumber.eq(ItemRecNumber),
-                    TransDate.eq(TransDate.MAX),
+            .select(ItemID, StockingUM, ItemIsInactive) # select node
+            .inner(InventoryCosts # inner node
+                .select(ItemRecNumber, TransDate, Quantity, CostAcctRecNumber, RecordType) # select node
+                .inner(InventoryCosts # inner node
+                    .select(ItemRecNumber, RecordType, TransDate.MAX) # select node
+                    .where(RecordType == 50) # where node
+                    .group(ItemRecNumber) # group node
+                ).on( # on node
+                    ItemRecNumber == ItemRecNumber,
+                    TransDate == TransDate.MAX,
                 )
-                .where(RecordType == 50)
-            ).on(
-                ItemRecordNumber.eq(ItemRecNumber),
+                .where(RecordType == 50) # where node
+            ) # inner results 
+            .on( # on node
+                ItemRecordNumber == ItemRecNumber,
             )
-            .where(
+            .where( # where node
+                # check self or inner join node if present
                 ItemIsInactive == 0,
                 CostAcctRecNumber == 61,
             )
-            .order(ItemRecNumber.ASC)
+            .order(ItemRecNumber.ASC) # order node
     )
 
-    print(sql)
+    nodes = select.ast_nodes
+    for node in nodes:
+        print(f'{node.node.alias} -> {node.syntax}')
+
+    #print(Query(select))
 
 
 
--- pypsql/structure.py
+++ pypsql/structure.py
@@ -1,15 +1,48 @@
 from __future__ import annotations
-from typing import Literal
-from dataclasses import dataclass
+from typing import Literal, Any
+from dataclasses import dataclass, field, InitVar
 
 
+class SQLContext:
+    def __init__(self):
+        self._count = 0
+        self.params: list[Any] = []
+        self._aliases: dict[Select | Inner, str] = {}
+    
+    def next_placeholder(self) -> str:
+        self._count += 1
+        return '?'
+    
+    def register_all_aliases(self, node: Select | Inner):
+        self._aliases[node] = node.alias
+        if node.join:
+            self._aliases[node.join] = node.join.alias
+            self.register_all_aliases(node.join.inner_select)
+    
+    def get_alias(self, node: Select | Inner) -> str:
+        try:
+            return self._aliases[node]
+        except KeyError:
+            raise ValueError(f"Unregistered node: {node!r}")
+        
+@dataclass(frozen=True, slots=True)
+class ASTNode:
+    syntax: Literal['select', 'where', 'group', 'order', 'inner', 'on']
+    node: Select | Inner
+        
+
+@dataclass(frozen=True, slots=True)
 class Filter:
-    def __init__(self, field: Field, comp_op, value):
-        self.field = field
-        self.comp_op = comp_op
-        self.value = value
+    field: Field
+    comp_op: Literal['=', '!=', '>', '>=', '<', '<=']
+    value: Any
+    origin: Select | Inner = field(default=None, init=False)
 
-@dataclass
+    def set_origin(self, origin: Select | Inner) -> None:
+        object.__setattr__(self, 'origin', origin)
+
+
+@dataclass(frozen=True, slots=True)
 class Order:
     field: Field
     direction: Literal['ASC', 'DESC']
@@ -17,16 +50,36 @@ class Order:
     def __post_init__(self):
         if self.direction not in ('ASC', 'DESC'):
             raise ValueError(f"Invalid Order direction: {self.direction}")
+        
 
+@dataclass(frozen=True, slots=True)
 class Field:
-    def __init__(self, name, type: Literal['number', 'text'], table: Table, create_variations=True):
-        self.table = table
-        self.name = name
-        self.type = type
-        self.MAX = MaxField(self) if create_variations else None
-        self.MIN = MinField(self) if create_variations else None
-        self.ASC = (self, f'?.{self.name} ASC')
-        self.DESC = (self, f'?.{self.name} DESC')
+    name: str
+    type: Literal['int', 'decimal', 'text']
+    table: Table
+    
+    create_variations: InitVar[bool] = True
+    
+    MAX: MaxField = field(init=False)
+    MIN: MinField = field(init=False)
+    ASC: Order = field(init=False)
+    DESC: Order = field(init=False)
+
+    def __post_init__(self, create_variations: bool):
+        if self.type not in ('int', 'decimal', 'text'):
+            raise ValueError(f'Invalid Field type: {self.type}')
+        if not isinstance(self.table, Table):
+            raise ValueError(f'Invalid Field table: {self.table}')
+        
+        if create_variations:
+            object.__setattr__(self, 'MAX', MaxField.from_original(self))
+            object.__setattr__(self, 'MIN', MinField.from_original(self))
+        else:
+            object.__setattr__(self, 'MAX', None)
+            object.__setattr__(self, 'MIN', None)
+
+        object.__setattr__(self, 'ASC', Order(self, 'ASC'))
+        object.__setattr__(self, 'DESC', Order(self, 'DESC'))
 
     def __eq__(self, other) -> Filter:
         return Filter(self, '=', other)
@@ -41,30 +94,45 @@ class Field:
     def __ge__(self, other) -> Filter:
         return Filter(self, '>=', other)
     
-    def resolve(self, alias):
-        if self.is_max:
+    def resolve(self, alias: str):
+        if isinstance(self, MaxField):
             return f'MAX({alias}.{self.original.name}) AS {alias}.{self.name}'
-        return f'{alias}.{self.name}'
-    
-class MaxField(Field):
-    def __init__(self, original: Field):
-        super().__init__(
-            name='Max'+original.name,
-            type=original.type,
-            table=original.table,
-            create_variations=False
-        )
-        self.original = original
+        if isinstance(self, MinField):
+            return f'MIN({alias}.{self.original.name}) AS {alias}.{self.name}'
+        if isinstance(self, Field):
+            return f'{alias}.{self.name}'
+        raise Exception('Unknown Field Type')
 
-class MinField(Field):
-    def __init__(self, original: Field):
-        super().__init__(
-            name='Min'+original.name,
+@dataclass(frozen=True, slots=True)
+class MaxField(Field):
+    original: Field = field(init=False)
+    
+    @classmethod
+    def from_original(cls, original: Field) -> MaxField:
+        inst = cls(
+            name='Max' + original.name,
             type=original.type,
             table=original.table,
             create_variations=False
         )
-        self.original = original
+        object.__setattr__(inst, 'original', original)
+        return inst
+    
+    
+@dataclass(frozen=True, slots=True)
+class MinField(Field):
+    original: Field = field(init=False)
+    
+    @classmethod
+    def from_original(cls, original: Field) -> MinField:
+        inst = cls(
+            name='Min' + original.name,
+            type=original.type,
+            table=original.table,
+            create_variations=False
+        )
+        object.__setattr__(inst, 'original', original)
+        return inst
 
 
 class Table:
@@ -83,6 +151,7 @@ class Select:
         # maybe hash and append refcount
         self.alias = f'{n[0]}{n[len(n)//2]}{n[-1]}{calling_table.ref_count}'
         self.fields = fields
+        self.ast_nodes: list[ASTNode] = [ASTNode('select', self)]
         self.wheres = None
         self.group_by = None
         self.order_by = None
@@ -95,14 +164,34 @@ class Select:
     
     def where(self, *wheres: Filter) -> Select:
         self.wheres = wheres
+        for f in self.wheres:
+            if self.join is None:
+                f.set_origin(self)
+                continue
+            if f.field in self.join.inner_select.fields:
+                f.set_origin(self.join)
+                continue
+            if f.field in self.fields:
+                f.set_origin(self)
+                continue
+            if f.field.table == self.calling_table:
+                f.set_origin(self)
+                continue
+            raise Exception('Cannot resolve filter origin for field: {f.field.name}')
+        self.ast_nodes.append(ASTNode('where', self))
         return self
+        # join nodes return columns based on inner select or the table it references 
+        # ON clauses left = parent select, right = result of join, aka the join node itself
+        # WHERE clauses after can either reference the left aka select, or right aka join node results
     
     def group(self, field: Field) -> Select:
         self.group_by = field
+        self.ast_nodes.append(ASTNode('group', self))
         return self
     
-    def order(self, *order_tups) -> Select:
-        self.order_by = order_tups
+    def order(self, *orders: Order) -> Select:
+        self.order_by = orders
+        self.ast_nodes.append(ASTNode('order', self))
         return self
     
     def resolve(self):
@@ -114,7 +203,7 @@ class Select:
                 query += field.resolve(self.alias) + ','
         return query[:-1] + f' FROM {self.calling_table.name} {self.alias}'
 
-
+# make Join base class and Inner Subclass
 class Inner:
     def __init__(self, parent_select: Select, inner_select: Select):
         self.parent_select = parent_select
@@ -123,9 +212,14 @@ class Inner:
         i = inner_select.calling_table.ref_count
         self.alias = f'{n[0]}{n[len(n)//2]}{n[-1]}{i}'
         self.ons = []
+        self.ast_nodes: list[ASTNode] = parent_select.ast_nodes
+        self.ast_nodes.append(ASTNode('inner', self))
+        self.ast_nodes.extend(inner_select.ast_nodes)
+        inner_select.ast_nodes = self.ast_nodes
 
     def on(self, *ons: Filter) -> Select:
         self.ons = ons
+        self.ast_nodes.append(ASTNode('on', self))
         return self.parent_select
     
 
@@ -167,7 +261,7 @@ def Query(select: Select):
                 for filter in part_v.wheres:
                     alias = get_alias(filter.field, part_v, part_v.join)
                     sel += f'{alias}.{filter.field.name} {filter.comp_op} '
-                    if filter.field.type == 'number':
+                    if filter.field.type == 'int' or filter.field.type == 'decimal':
                         sel += f'{filter.value} AND '
                     elif filter.field.type == 'text':
                         sel += f"'{filter.value}' AND "
@@ -177,8 +271,8 @@ def Query(select: Select):
             if part_v.order_by is not None:
                 sel += '\nORDER BY\n'
                 for order in part_v.order_by:
-                    alias = get_alias(order[0], part_v, part_v.join)
-                    sel += f'{order[1].replace('?', alias)} AND '
+                    alias = get_alias(order.field, part_v, part_v.join)
+                    sel += f'{alias}.{order.field.name} {order.direction} AND '
                 sel = sel[0:-5]
             if sql_parts:
                 before = sql_parts.pop()
